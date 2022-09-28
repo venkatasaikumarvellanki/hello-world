65627c746effd73144a907566cd922399e65d419
venkatasaikumarvellanki <95701223+venkatasaikumarvellanki@users.noreply.github.com>
2022-09-28 15:00:48 +0530

    Add files via upload

---
+++ Hello world in c++.cpp
@@ -0,0 +1,13 @@
+/*             in c programming                                  in c++
+ 
+ input         scanf("%d",&a)                             cin>> (insertion)     (console input)
+ output       printf("%d",a)                              cout<< (extraction)   (console output)
+             //formatted input         */
+             
+//printing of "hello world" 
+#include<iostream>
+using namespace std;
+main()
+{
+	std::cout<<"HELLO WORLD";
+}
